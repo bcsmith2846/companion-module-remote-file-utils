12dcd0a59379342a04cf61196c0ae05147aff28c
Brenden Smith <brenden@brendensmith.net>
2025-03-10 18:47:09 -0500

    Add scripts for major and patch releases. Add a `v` prefix to the release title. Added commit & push to release scripts.

---
+++ scripts/release-next-minor.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+echo --% >/dev/null;: '           | out-null
+<#'
+
+NEXT_VERSION=$(./scripts/next-minor.ps1)
+./scripts/update-version.ps1  ${NEXT_VERSION}
+echo "Updated version to ${NEXT_VERSION}"
+
+git add ./package.json ./companion/manifest.json
+git commit -m "Update version to ${NEXT_VERSION}"
+git push
+echo "Committed and pushed version update"
+
+git tag -s -m "v${NEXT_VERSION} release" "v${NEXT_VERSION}"
+echo "Added tag v${NEXT_VERSION}"
+
+git push origin tag "v${NEXT_VERSION}"
+echo "Pushed tag v${NEXT_VERSION} to remote"
+
+exit #>
+
+$NEXT_VERSION = $(./scripts/next-minor.ps1)
+./scripts/update-version.ps1  ${NEXT_VERSION}
+echo "Updated version to ${NEXT_VERSION}"
+
+git add ./package.json ./companion/manifest.json
+git commit -m "Update version to ${NEXT_VERSION}"
+git push
+echo "Committed and pushed version update"
+
+git tag -s -m "v${NEXT_VERSION} release" "v${NEXT_VERSION}"
+echo "Added tag v${NEXT_VERSION}"
+
+git push origin tag "v${NEXT_VERSION}"
+echo "Pushed tag v${NEXT_VERSION} to remote"
